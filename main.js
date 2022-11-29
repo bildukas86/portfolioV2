20316d351d2d02f393cba6c3e6f5b1103720d616
bildukas86 <ekoncevicius@gmail.com>
2022-11-29 02:20:43 +0000

    test
    test

--- main.js
+++ main.js
@@ -4,23 +4,23 @@ function hamFunction(x) {
 
   x.classList.toggle("change");
 
-  // var s = document.getElementById('sideNav');
-  // var b = document.body;
-  // var m = document.getElementById('main-container');
+  var s = document.getElementById('sideNav');
+  var b = document.body;
+  var m = document.getElementById('main-container');
+
+  
+    if (s.style.display === "block"){
+     s.style.display = "none";
+     b.style.overflow = "visible";
+     b.style.left = "0";
+     m.style.opacity = "1";
+
+    } else {
+      s.style.display = "block";
+      b.style.overflow = "hidden";
+      b.style.left = "-275px";
+      m.style.opacity = "0.3";
 
 
-    // if (s.style.display === "block"){
-    //  s.style.display = "none";
-    //  b.style.overflow = "visible";
-    //  b.style.left = "0";
-    //  m.style.opacity = "1";
-    //
-    // } else {
-    //   s.style.display = "block";
-    //   b.style.overflow = "hidden";
-    //   b.style.left = "-275px";
-    //   m.style.opacity = "0.3";
-    //
-    //
-    // }
+    }
 }

862d478a5c3678584d63c7ff9cc66fa8301b9c11
bildukas86 <ekoncevicius@gmail.com>
2022-12-01 14:03:00 +0000

    fix/add
    fixed menu, nave styled, adding scheme and coding and about page.

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 
-//animated hamburger icon
-function hamFunction(x) {
-
+function hamFunction(x){
+  //add class
   x.classList.toggle("change");
-  
-//hide /unhide nav list
-  var n = document.getElementsByClassName("navigation");
 
+  // open/close navigation
+  var n = document.getElementById('nav');
   if (n.style.display === "block"){
-    n.style.display = "none";
-  } else {
-    n.style.display = "block";
-  }
-
+       n.style.display = "none";
+     } else {
+       n.style.display = "block";
+     }
 }
